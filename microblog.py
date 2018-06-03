e1d8b00b4e81078248c8a07c5ff7d7650e38328d
Nicolay Poymanov <n.poymanov@gmail.com>
2018-06-03 18:11:25 +0300

    Add basa models

--- microblog.py
+++ microblog.py
@@ -1,1 +1,7 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_proccesor
+def make_shell_context():
+	return {'db': db, 'User': User, 'Post': Post}
+

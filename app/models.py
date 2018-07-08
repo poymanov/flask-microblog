1c1cf579f3ba1e9246c35d89ba4619827ae04cb3
Nicolay Poymanov <n.poymanov@gmail.com>
2018-07-08 18:46:09 +0300

    Add API func

--- app/models.py
+++ app/models.py
@@ -10,13 +10,40 @@ import jwt
 import json
 import redis
 import rq
+from flask import url_for
+import base64
+from datetime import datetime, timedelta
+import os
 
 followers = db.Table('followers', 
 	db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
 	db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
 )
 
-class User(db.Model, UserMixin):
+class PaginatedAPIMixin(object):
+	@staticmethod
+	def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+		resources = query.paginate(page, per_page, False)
+		data = {
+			'items': [item.to_dict() for item in resources.items],
+			'_meta': {
+				'page': page,
+				'per_page': per_page,
+				'total_pages': resources.pages,
+				'total_items': resources.total
+			},
+			'_links': {
+				'self': url_for(endpoint, page=page, per_page=per_page, **kwargs),
+				'next': url_for(endpoint, page=page + 1, per_page=per_page,
+					**kwargs) if resources.has_next else None,
+				'prev': url_for(endpoint, page=page - 1, per_page=per_page,
+					**kwargs) if resources.has_prev else None				
+			}
+		}
+
+		return data
+
+class User(PaginatedAPIMixin, UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), index=True, unique = True)
 	email = db.Column(db.String(120), index=True, unique = True)
@@ -38,6 +65,8 @@ class User(db.Model, UserMixin):
 	last_message_read_time = db.Column(db.DateTime)
 	notifications = db.relationship('Notification', backref='user', lazy='dynamic')
 	tasks = db.relationship('Task', backref='user', lazy='dynamic')
+	token = db.Column(db.String(32), index=True, unique=True)
+	token_expiration = db.Column(db.DateTime)
 
 	def __repr__(self):
 		return 'User {}'.format(self.username)
@@ -101,6 +130,54 @@ class User(db.Model, UserMixin):
 		return Task.query.filter_by(name=name, user=self,
 									complete=False).first()		
 
+	def to_dict(self, include_email=False):
+		data = {
+			'id': self.id,
+			'username': self.username,
+			'last_seen': self.last_seen.isoformat() + 'Z',
+			'about_me': self.about_me,
+			'post_count': self.posts.count(),
+			'follower_count': self.followers.count(),
+			'followed_count': self.followed.count(),
+			'_links': {
+				'self': url_for('api.get_user', id=self.id),
+				'followers': url_for('api.get_followers', id=self.id),
+				'followed': url_for('api.get_followed', id=self.id),
+				'avatar': self.avatar(128)
+			}
+		}
+
+		if include_email:
+			data['email'] = self.email
+
+		return data
+
+	def from_dict(self, data, new_user=False):
+		for field in ['username', 'email', 'about_me']:
+			if field in data:
+				setattr(self, field, data[field])
+		if new_user and 'password' in data:
+			self.set_password(data['password'])
+
+	def get_token(self, expires_in=3600):
+		now = datetime.utcnow()
+		if self.token and self.token_expiration > now + timedelta(seconds=60):
+			return self.token
+		self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+		self.token_expiration = now + timedelta(seconds=expires_in)
+		db.session.add(self)
+		return self.token
+
+	def revoke_token(self):
+		self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+	@staticmethod
+	def check_token(token):
+		user = User.query.filter_by(token=token).first()
+		if user is None or user.token_expiration < datetime.utcnow():
+			return None
+		return user
+
 	@staticmethod	
 	def verify_reset_password_token(token):
 		try:
@@ -109,7 +186,6 @@ class User(db.Model, UserMixin):
 			return
 		return User.query.get(id)	
 			
-
 class SearchableMixin(object):
     @classmethod
     def search(cls, expression, page, per_page):
